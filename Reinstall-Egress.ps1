f5a009a39b0084ecc81c210c58de5a73f2b58788
gwilliam0021 <31669045+gwilliam0021@users.noreply.github.com>
2023-11-15 13:58:49 -0500

    Add files via upload

---
+++ Reinstall-Egress.ps1
@@ -0,0 +1,4 @@
+﻿wmic product where 'name = "Egress"' call uninstall | Out-Default
+start-sleep -seconds 30
+invoke-webrequest -uri "https://www.egress.com/media/adenkiml/egressemailprotection-23-09-1371.msi" -OutFile "C:\temp\egress.msi"
+msiexec /i "C:\temp\egress.msi" /qn
