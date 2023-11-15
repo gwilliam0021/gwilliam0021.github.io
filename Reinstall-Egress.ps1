6e493c697fddc03beb4f14a5482658a0ab8d4f08
gwilliam0021 <31669045+gwilliam0021@users.noreply.github.com>
2023-11-15 14:02:53 -0500

    Update Reinstall-Egress.ps1

--- Reinstall-Egress.ps1
+++ Reinstall-Egress.ps1
@@ -1,4 +1,4 @@
-﻿wmic product where 'name = "Egress"' call uninstall | Out-Default
+﻿wmic product where 'name = "Egress"' call uninstall /nointeractive | Out-Default
 start-sleep -seconds 30
 invoke-webrequest -uri "https://www.egress.com/media/adenkiml/egressemailprotection-23-09-1371.msi" -OutFile "C:\temp\egress.msi"
 msiexec /i "C:\temp\egress.msi" /qn
